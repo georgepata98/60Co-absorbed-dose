e5ce0fc88c4de07f06f733e0ea4e7652e786efe1
George <122010570+georgepata98@users.noreply.github.com>
2024-07-26 20:34:12 +0300

    Add files via upload

---
+++ include/run.hh
@@ -0,0 +1,35 @@
+#ifndef RUN_HH
+#define RUN_HH
+
+#include "G4SystemOfUnits.hh"
+#include "G4UserRunAction.hh"
+#include "G4AnalysisManager.hh"
+#include "G4Run.hh"
+#include "G4RunManager.hh"
+#include "G4UnitsTable.hh"
+#include "G4Accumulable.hh"
+#include "G4AccumulableManager.hh"
+
+#include <iostream>
+#include <fstream>
+
+class MyRunAction : public G4UserRunAction
+{
+public:
+    MyRunAction();
+    ~MyRunAction();
+
+    virtual void BeginOfRunAction(const G4Run* run);
+    virtual void EndOfRunAction(const G4Run* run);
+
+    void AddDdep(G4double dose) { totalDose += dose; }
+
+    std::ofstream* GetOutputFile();
+
+private:
+    G4Accumulable<G4double> totalDose = 0;
+
+    std::ofstream fOutputFile;
+};
+
+#endif
